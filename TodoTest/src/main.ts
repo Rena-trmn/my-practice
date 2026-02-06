f7079960748163c70e550dbd7447ceb7e412f835
Rena-trmn <iwasaki@two-six.jp>
2026-02-06 11:33:27 +0900

    修正しました

--- TodoTest/src/main.ts
+++ TodoTest/src/main.ts
@@ -1,19 +1,55 @@
 import './style.css'
 
-export type todos = {
-  id:number,
-  text:string,
+//型の定義
+interface Todo {
+  id:number;
+  text:string;
 }
 
-const input = document.getElementById("text-input") as HTMLInputElement;
-const button = document.getElementById("input-button") as HTMLButtonElement;
-const todoList = document.getElementById("todo-list") as HTMLUListElement;
-const deleteList = document.getElementById("delete") as HTMLButtonElement;
+//DOM取得
+const input = document.getElementById("todo-input") as HTMLInputElement;
+const addBtn = document.getElementById("add-btn") as HTMLButtonElement;
+const list = document.getElementById("todo-list") as HTMLUListElement;
 
-button.addEventListener('click',()=> {
-  const Todo = input.value;
-  if (Todo.trim()==="") return;
-  todoList.appendChild(ul);
+//TODOを保存する配列
+let todos : Todo[] = [];
 
-});
+//画面描画
+function renderTodos():void {
+  list.innerHTML = "";
 
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.textContent = todo.text;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "削除";
+    deleteBtn.onclick = () =>deleteTodo(todo.id);
+
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+  });
+
+}
+
+//TODOの追加
+function addTodo():void{
+  if (input.value === "")return;
+
+  const newTodo:Todo = {
+    id: Date.now(),
+    text:input.value,
+  }
+  
+  todos.push(newTodo);
+  input.value = "";
+  renderTodos();
+}
+
+//TODOの削除
+function deleteTodo(id:number):void {
+  todos = todos.filter((todo) => todo.id !== id);
+  renderTodos();
+}
+
+addBtn.addEventListener("click",addTodo);
